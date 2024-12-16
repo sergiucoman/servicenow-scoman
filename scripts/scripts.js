a671bcd7365599779a92696a16b0bffb1fba1c6c
Sergiu Coman <scoman@Sergius-MacBook-Pro-3.local>
2024-12-16 17:00:42 +0200

    Patch linked pictures

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -63,6 +63,8 @@ export function decorateMain(main) {
   // hopefully forward compatible button decoration
   decorateButtons(main);
   decorateIcons(main);
+  // eslint-disable-next-line no-use-before-define
+  decorateLinkedPictures(main);
   buildAutoBlocks(main);
   decorateSections(main);
   decorateBlocks(main);
@@ -128,3 +130,17 @@ async function loadPage() {
 }
 
 loadPage();
+
+/**
+ * Decorates linked pictures in a given block.
+ * @param {HTMLElement} block - The block element containing the pictures.
+ */
+function decorateLinkedPictures(block) {
+  block.querySelectorAll('picture + br + a').forEach((a) => {
+    // remove br
+    a.previousElementSibling.remove();
+    const picture = a.previousElementSibling;
+    a.textContent = '';
+    a.append(picture);
+  });
+}
